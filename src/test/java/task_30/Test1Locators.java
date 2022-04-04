8d1178f1aca5d5ae2ddf4468bb072507be58b667
pavel-kostoglod <pavelkostoglod@coherentsolutions.com>
2022-04-04 17:37:13 +0300

    - remove '_' from classes and variables names - make constants 'static' in the Test1Locators class

---
+++ src/test/java/task_30/Test1Locators.java
@@ -0,0 +1,13 @@
+package task_30;
+
+import org.openqa.selenium.By;
+
+public class Test1Locators {
+    static final By.ByXPath FIRST_LOGIN_BUTTON = new By.ByXPath("//*[contains(text(), 'Log in')]/ancestor::a");
+    static final By.ByName EMAIL_INPUT = new By.ByName("login");
+    static final By.ByCssSelector PASS_INPUT = new By.ByCssSelector("input[name='passwd']");
+    static final By.ByClassName SUBMIT_BUTTON = new By.ByClassName("Button2_type_submit");
+    static final By.ById PHONE_INPUT = new By.ById("passp-field-phoneNumber");
+
+
+}
