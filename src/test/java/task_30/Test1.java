2e22ec7d67657e0438c1e77aebb222b862cccd47
pavel-kostoglod <pavelkostoglod@coherentsolutions.com>
2022-04-06 19:11:58 +0300

    - tasks from 1 to 4 are done in the old one task_30.Test1 - all other tasks are done in new package task_40 in separate classes

--- src/test/java/task_30/Test1.java
+++ src/test/java/task_30/Test1.java
@@ -3,17 +3,21 @@ package task_30;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 public class Test1 {
     private final String CHROME_DRIVER_PATH = "src/test/resources/chromedriver";
-    private String EMAIL = "automationtester.p";
-    private String PASSWORD = "automationtester";
+    private final String URL = "https://mail.yandex.com";
+    private final int WAIT = 10;
+    private final int POOLING_FREQUENCY = 1000;
     private WebDriver driver;
     private WebDriverWait wait;
 
@@ -21,24 +25,31 @@ public class Test1 {
     void setup() {
         System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH);
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 3);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // implicit waiter
+        wait = new WebDriverWait(driver, WAIT, POOLING_FREQUENCY); // explicit waiter initialization
     }
 
-    @Test
-    void test1() {
-        driver.get("https://mail.yandex.com");
+    @ParameterizedTest
+    @CsvFileSource(resources = "/credentials.csv")
+    void test1(String email, String pass) {
+        driver.get(URL);
         driver.findElement(Test1Locators.FIRST_LOGIN_BUTTON).click();
         WebElement loginInput = driver.findElement(Test1Locators.EMAIL_INPUT);
-        loginInput.sendKeys(EMAIL);
+        loginInput.sendKeys(email);
         driver.findElement(Test1Locators.SUBMIT_BUTTON).click();
-        wait.until(ExpectedConditions.presenceOfElementLocated(Test1Locators.PASS_INPUT));
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) { // "native" programming language explicit waiter
+            e.printStackTrace();
+        }
+        wait.until(ExpectedConditions.presenceOfElementLocated(Test1Locators.PASS_INPUT)); // explicit waiter usage
         WebElement passInput = driver.findElement(Test1Locators.PASS_INPUT);
-        passInput.sendKeys(PASSWORD);
+        passInput.sendKeys(pass);
         driver.findElement(Test1Locators.SUBMIT_BUTTON).click();
 
-        wait.until(ExpectedConditions.presenceOfElementLocated(Test1Locators.PHONE_INPUT));
+        wait.until(ExpectedConditions.presenceOfElementLocated(Test1Locators.USER_NAME_LABEL)); // explicit waiter usage
 
-        Assertions.assertTrue(driver.findElement(Test1Locators.PHONE_INPUT).isDisplayed());
+        Assertions.assertTrue(driver.findElement(Test1Locators.USER_NAME_LABEL).isEnabled());
     }
 
     @AfterEach
--- src/test/java/task_30/Test1Locators.java
+++ src/test/java/task_30/Test1Locators.java
@@ -7,7 +7,5 @@ public class Test1Locators {
     static final By.ByName EMAIL_INPUT = new By.ByName("login");
     static final By.ByCssSelector PASS_INPUT = new By.ByCssSelector("input[name='passwd']");
     static final By.ByClassName SUBMIT_BUTTON = new By.ByClassName("Button2_type_submit");
-    static final By.ById PHONE_INPUT = new By.ById("passp-field-phoneNumber");
-
-
+    static final By.ByXPath USER_NAME_LABEL = new By.ByXPath("//*[contains(@class, 'user-pic')]/preceding-sibling::span[@class='user-account__name']");
 }
