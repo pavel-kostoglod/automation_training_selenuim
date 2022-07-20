33cc8960f3e10f6674a6c58465dd3797d8497190
pavel-kostoglod <pavelkostoglod@coherentsolutions.com>
2022-07-20 21:22:38 +0200

    - Selenium grid

--- src/test/java/utils/WebDriverSingleton.java
+++ src/test/java/utils/WebDriverSingleton.java
@@ -2,7 +2,12 @@ package utils;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import task_60_2.TestConstants;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class WebDriverSingleton {
 
@@ -12,8 +17,12 @@ public class WebDriverSingleton {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            System.setProperty("webdriver.chrome.driver", TestConstants.CHROME_DRIVER_PATH);
-            driver = new ChromeDriver();
+//            System.setProperty("webdriver.chrome.driver", TestConstants.CHROME_DRIVER_PATH);
+            try {
+                driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.chrome());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
         }
         return driver;
     }
